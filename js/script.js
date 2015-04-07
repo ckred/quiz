8bc41745b3897458583787e144c928c8b55ad3e6
Caryl Kristen Dizon <carylkristen@gmail.com>
2015-04-07 16:06:17 -0400

    nybg troub

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		qnum = 1; // RESET CURRENT QUESTION NUMBER
 		index = qnum - 1;
 
-		$(".theAnswerSpace").css('background-image', 'url("' + backgrounds[index] + '")');
+		$(".theAnswerSpace").css('background-image', 'url("images/NY.png")');
 
 	// RESET ANSWERS
 		$("ul#answers").empty();
